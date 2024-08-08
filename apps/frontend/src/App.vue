fbff49023434c7a81bffd689e60c3b1ab18278bf
ZurekMartin <zurek.martin.1@icloud.com>
2024-08-08 17:40:43 +0200

    add info icon

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -7,6 +7,9 @@ import Calculator from './components/Calculator.vue';
 const isHistoryVisible = ref(false);
 const isMenuVisible = ref(false);
 const isDarkMode = ref(false);
+const isInfoLabelVisible = ref(false);
+const infoMessage = ref('');
+const infoIconClickCount = ref(0);
 
 function toggleHistory() {
   isHistoryVisible.value = !isHistoryVisible.value;
@@ -15,18 +18,40 @@ function toggleHistory() {
 function toggleMenu() {
   isMenuVisible.value = !isMenuVisible.value;
 }
+
+function toggleInfo() {
+  infoIconClickCount.value++;
+
+  if (infoIconClickCount.value === 8) {
+    infoMessage.value = 'Surprise!';
+    infoIconClickCount.value = 0;
+  } else {
+    infoMessage.value = 'Calculator Version: 1.0';
+  }
+
+  isInfoLabelVisible.value = true;
+
+  setTimeout(() => {
+    isInfoLabelVisible.value = false;
+  }, 4000);
+}
 </script>
 
 <template>
   <div id="app" :class="{ 'dark-theme': isDarkMode }">
-    <button @click="toggleHistory" id="history-toggle" :class="{ 'dark-icon': isDarkMode }">
-      <img :src="isDarkMode ? 'src/assets/history_dark_mode.png' : 'src/assets/history.png'" alt="Menu Icon" />
+    <button @click="toggleHistory" id="history-icon" :class="{ 'dark-icon': isDarkMode }">
+      <img class="icon" :src="isDarkMode ? 'src/assets/history_dark_mode.png' : 'src/assets/history.png'"
+           alt="History Icon"/>
     </button>
-    <History :class="{ visible: isHistoryVisible }" />
-    <button @click="toggleMenu" id="menu-toggle" :class="{ 'dark-icon': isDarkMode }">
-      <img :src="isDarkMode ? 'src/assets/menu.png' : 'src/assets/menu.png'" alt="Menu Icon" />
+    <History :class="{ visible: isHistoryVisible }"/>
+    <button @click="toggleMenu" id="menu-icon" :class="{ 'dark-icon': isDarkMode }">
+      <img class="icon" :src="isDarkMode ? 'src/assets/menu.png' : 'src/assets/menu.png'" alt="Menu Icon"/>
     </button>
-    <Menu :class="{ visible: isMenuVisible }" />
-    <Calculator />
+    <Menu :class="{ visible: isMenuVisible }"/>
+    <Calculator/>
+    <button @click="toggleInfo" id="info-icon" :class="{ 'dark-icon': isDarkMode }">
+      <img class="icon" :src="isDarkMode ? 'src/assets/info_dark_mode.png' : 'src/assets/info.png'" alt="Info Icon"/>
+    </button>
+    <span id="infoLabel" :class="{ visible: isInfoLabelVisible }">{{ infoMessage }}</span>
   </div>
 </template>
